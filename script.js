5c88a81390f1cc56521dc706ed261574f97fd95f
root0217 <pornoananas@gmail.com>
2025-06-27 12:29:52 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const music = document.getElementById('bgMusic');
+const playBtn = document.getElementById('playBtn');
+
+function showNextSlide() {
+  slides[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000);
+
+playBtn.addEventListener('click', () => {
+  music.play();
+  playBtn.style.display = 'none';
+});
